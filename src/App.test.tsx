d94ffb9e7d87ce6cc610878cac79f09f962d2dab
sergevitali <kangurularfalan@gmail.com>
2022-10-20 15:20:01 +1100

    React App Boilerplate with Typescript, MUI, Sass, Webpack

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders App', () => {
+  render(<App />)
+  const h1Element = screen.getByText('This is an app')
+  const buttonElement = screen.getByRole('button', { name: 'Hello World' })
+  expect(h1Element).toBeInTheDocument()
+  expect(buttonElement).toBeInTheDocument()
+})
