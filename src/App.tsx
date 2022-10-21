70dbed501647b39e6fbc6b68d511ebbdcf528475
sergevitali <kangurularfalan@gmail.com>
2022-10-21 13:00:31 +1100

    table

--- src/App.tsx
+++ src/App.tsx
@@ -43,18 +43,39 @@ const recursiveFlat = (object: Location, array: string[], mainKey?: string) => {
   })
 }
 
+const flattenLocationsHeaders = (locations: Location[]): any => {
+  const flattenedLocationsHeaders: string[] = []
+  recursiveFlat(locations[0], flattenedLocationsHeaders)
+  return flattenedLocationsHeaders
+}
+
+const flattenLocationsValues = (locations: Location[]): any => {
+  const flattenedLocationValues: string[] = []
+  for (const { street, coordinates, timezone, ...rest } of locations) {
+    flattenedLocationValues.push({
+      ...rest,
+      'street-name': street.name,
+      'street-number': street.number,
+      'coordinates-latitude': coordinates.latitude
+    })
+  }
+  const newSortedValues = { ...flattenedLocationValues }
+  // newSortedValues.sort((a: string, b: string) => {
+  //   if (a <)
+  //   return 0
+  // })
+  return flattenedLocationValues
+}
+
 function App() {
   const [people, setPeople] = useState<Person[]>([])
-  const [locationsHeaders, setLocationHeaders] = useState<Location[]>([])
+  const [locationsHeaders, setLocationHeaders] = useState<string[]>([])
+  const [locationValues, setLocationValues] = useState<any[]>()
+
+  const [showLoginForm, setShowLoginForm] = useState<boolean>(false)
 
   const [loading, setLoading] = useState<boolean>(true)
   const [reFetch, setRefetch] = useState<boolean>(false)
-
-  const flattenLocations = (locations: Location[]): any => {
-    const flattenedLocationsHeaders: string[] = []
-    recursiveFlat(locations[0], flattenedLocationsHeaders)
-    return flattenedLocationsHeaders
-  }
 
   useEffect(() => {
     let mounted = true
@@ -65,7 +86,10 @@ function App() {
           if (mounted) {
             setPeople(data)
             setLocationHeaders(
-              flattenLocations(data.map(({ location }) => location))
+              flattenLocationsHeaders(data.map(({ location }) => location))
+            )
+            setLocationValues(
+              flattenLocationsValues(data.map(({ location }) => location))
             )
             setLoading(false)
           }
@@ -86,22 +110,64 @@ function App() {
 
   if (people) {
     return (
-      <div className={Styles.App}>
+      <Box className={Styles.App}>
         <h1>This is an app</h1>
         <Button
           variant="contained"
           className={Styles.button}
-          onClick={() => setRefetch(!reFetch)}
+          onClick={() => setShowLoginForm(!showLoginForm)}
         >
-          Hello World
+          Login Form Toggle
         </Button>
-        {people?.map((person, personIdx) => (
-          <p key={personIdx}>{person.name.first}</p>
-        ))}
-        {locationsHeaders.map((header, index) => (
-          <div key={index}>{header}</div>
-        ))}
-      </div>
+        {showLoginForm && (
+          <Box sx={{ margin: 'auto', width: 400, padding: 10 }}>
+            <form style={{ width: '400px' }}>
+              <input
+                style={{ marginBottom: '10px', padding: '10px', width: '100%' }}
+                name="name"
+              />
+              <input
+                style={{ width: '100%', padding: '10px' }}
+                name="passowrd"
+              />
+            </form>
+          </Box>
+        )}
+        {!showLoginForm && (
+          <>
+            <Button
+              variant="contained"
+              className={Styles.button}
+              onClick={() => setRefetch(!reFetch)}
+            >
+              Hello World
+            </Button>
+            {people?.map((person, personIdx) => (
+              <p key={personIdx}>{person.name.first}</p>
+            ))}
+            {locationValues && (
+              <table>
+                <thead>
+                  <tr>
+                    {Object.keys(locationValues[0]).map((key) => (
+                      <th key={key + 'head'}>{key}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {locationValues?.map((location, idx) => (
+                    <tr key={idx + 'tr'}>
+                      {Object.values(location).map((value, inx) => (
+                        <td key={idx + 'td' + inx}>{'value'}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </>
+        )}
+      </Box>
     )
   }
 
